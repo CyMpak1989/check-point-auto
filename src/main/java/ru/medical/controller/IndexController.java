0ab54a6f3a1a850c099898f5e5d9cc53ec5d21c2
CyMpak1989 <cympak2009@mail.ru>
2018-12-10 23:20:48 +0300

    Коммит

--- src/main/java/ru/medical/controller/IndexController.java
+++ src/main/java/ru/medical/controller/IndexController.java
@@ -2,12 +2,13 @@ package ru.medical.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.medical.domain.Car;
 import ru.medical.repository.CarRepository;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -15,7 +16,7 @@ public class IndexController {
     @Autowired
     private CarRepository carRepository;
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public String index(Map<String, Object> model) {
 
         Iterable<Car> carAll = carRepository.findAll();
@@ -34,6 +35,20 @@ public class IndexController {
         Iterable<Car> carAll = carRepository.findAll();
 
         model.put("car", carAll);
+
+        return "index";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Car> car;
+        if (filter != null && !filter.isEmpty()) {
+            car = carRepository.findByNumberContaining(filter);
+        } else {
+            car = carRepository.findAll();
+        }
+
+        model.put("car", car);
 
         return "index";
     }
--- src/main/java/ru/medical/repository/CarRepository.java
+++ src/main/java/ru/medical/repository/CarRepository.java
@@ -3,6 +3,9 @@ package ru.medical.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.medical.domain.Car;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
+import java.util.List;
 
+public interface CarRepository extends CrudRepository<Car, Long> {
+    List<Car> findByNumber(String number);
+    List<Car> findByNumberContaining(String number);
 }
