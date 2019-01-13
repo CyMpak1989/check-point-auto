e343723c8bcb419b1e1fe925e8686bc739c301f4
CyMpak1989 <cympak2009@mail.ru>
2019-01-13 23:41:05 +0300

    Немного фиксов

--- src/main/java/ru/medical/controller/AddController.java
+++ src/main/java/ru/medical/controller/AddController.java
@@ -1,10 +1,12 @@
 package ru.medical.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.medical.domain.Car;
 import ru.medical.domain.User;
@@ -13,6 +15,8 @@ import ru.medical.repository.CarRepository;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/add")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class AddController {
 
     @Autowired
